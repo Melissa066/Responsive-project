72c0f63f7133346efb78adf5369ec8b82649b161
Melissa066 <150598956+Melissa066@users.noreply.github.com>
2024-01-31 21:50:53 +0000

    :+1:

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+// NAVBAR
+const navbar = document.querySelector(".navbar")
+
+window.addEventListener("scroll", ()=>{
+
+    if(window.scrollY > 0) {
+    
+    navbar.classList.add("sticky")
+} else {
+    navbar.classList.remove("sticky")
+    }
+})
+// END OF NAVBAR
+
+// PROJECTS
+
+const filterLinks = document.querySelectorAll('.filter-nav-link')
+
+filterLinks.forEach((filterLink) => {
+  filterLink.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    document.querySelector('.filter-nav-link.active').classList.remove('active')
+    filterLink.classList.add('active')
+
+    const projects = document.querySelectorAll('.project')
+    projects.forEach((project) => {
+      project.classList.add('hide')
+
+      if (
+        filterLink.getAttribute('data-type') === project.getAttribute('data-type') ||
+        filterLink.getAttribute('data-type') === 'all'
+      ) {
+        project.classList.remove('hide')
+      }
+    })
+  })
+})
+// END OF PROJECTS
